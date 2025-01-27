dc3e77e3ec52c4a17ad8bef5a03e4c6c1fd2b5d2
Muhammad Sohaib <164145499+Sohaib2005@users.noreply.github.com>
2025-01-27 21:44:47 +0500

    Create Q4.cpp

---
+++ SEM 02/OOP Lab/Lab 01/Q4.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    float weight;
+    cout<<"Enter weight" <<endl;
+    cin >> weight;
+    cout << weight << " kg = " <<( weight*2.2 )<< " pounds." << endl;
+    return 0;
+}
