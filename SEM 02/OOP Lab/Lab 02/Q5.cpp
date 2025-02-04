a0c10a4ae5b8f1804e90288ca49251c8c0812ba3
Muhammad Sohaib <164145499+Sohaib2005@users.noreply.github.com>
2025-02-04 09:45:39 +0500

    Update Q5.cpp

--- SEM 02/OOP Lab/Lab 02/Q5.cpp
+++ SEM 02/OOP Lab/Lab 02/Q5.cpp
@@ -20,28 +20,38 @@ int main() {
     for(int i = 0; i < n - 1 ;i++) {
         emp temp;
         for(int j = 0; j < n - 1 - i; j++) {
-            if(e[j].id > e[j+1].id)
+            if(e[j].id > e[j+1].id) {
             temp = e[j];
             e[j] = e[j+1];
             e[j+1] = temp;
+            }
+        if(e[j].id == e[j+1].id) {
+            if(e[j].name[0] > e[j+1].name[0]) {
+            temp = e[j];
+            e[j] = e[j+1];
+            e[j+1] = temp;
+            }
         }
-    }
-    for(int i = 0; i < n; i++) {
-       cout << "Employee id : " << e[i].id << "\n"
-            << "Employee name : " << e[i].name << "\n"; 
-    }
-    for(int i = 0; i < n - 1;i++) {
-            emp temp;
-        for(int j = 0; j < n - 1 - i; j++) {
-            if(e[j].name[0] > e[j+1].name[0])
-            temp = e[j];
-            e[j] = e[j+1];
-            e[j+1] = temp;
+        
         }
     }
     for(int i = 0; i < n; i++) {
       cout << "Employee id : " << e[i].id << "\n"
             << "Employee name : " << e[i].name << "\n"; 
     }
+    // for(int i = 0; i < n - 1;i++) {
+    //         emp temp;
+    //     for(int j = 0; j < n - 1 - i; j++) {
+    //         if(e[j].name[0] > e[j+1].name[0]) {
+    //         temp = e[j];
+    //         e[j] = e[j+1];
+    //         e[j+1] = temp;
+    //         }
+    //     }
+    // }
+    // for(int i = 0; i < n; i++) {
+    //   cout << "Employee id : " << e[i].id << "\n"
+    //         << "Employee name : " << e[i].name << "\n"; 
+    // }
     return 0;
 }
