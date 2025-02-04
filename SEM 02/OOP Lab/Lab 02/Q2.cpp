a2c30a4b367b5304bc18854956d8a9d1435d721a
Muhammad Sohaib <164145499+Sohaib2005@users.noreply.github.com>
2025-02-04 11:19:44 +0500

    Update Q2.cpp

--- SEM 02/OOP Lab/Lab 02/Q2.cpp
+++ SEM 02/OOP Lab/Lab 02/Q2.cpp
@@ -1,22 +1,28 @@
 #include <iostream>
-#include<string>
-using namespace std;
-void* addarr(int *s,int count) {
-        for(int i = 1; i < count;i++) {
-            s[i] += i;
-        }
-        return (void*)s;
+#include <string>
+#include <vector>
+
+void* addarr(void* nums, int count) {
+    int* total { new int(0) };
+    for(int i = 0; i < count; i++) {
+        *total += static_cast<int*>(nums)[i];
     }
-int main(int count,char *arr[]) {
-    int s[count - 1];
-    for(int i = 1; i < count ; i++) {
-     s[i - 1] = stoi(arr[i]);    
+    return static_cast<void*>(total);
+}
+
+int main(int argc, char *argv[]) {
+    // int* nums { new int[argc - 1] };
+    std::vector<int> nums {};
+    for(int i = 1; i < argc; i++) {
+        nums.push_back(std::stoi(argv[i]));
     }
-    
-    cout << addarr(s, count)<<endl;
-    for( int i = 0; i < count - 1; i++) {
-        cout << s[i] << " ";
+
+    int* total { static_cast<int*>(addarr(static_cast<void*>(&nums[0]), nums.size())) };
+    std::cout << "Total: " << *total << '\n';
+    for(int x : nums) {
+        std::cout << x << ' ';
     }
-    
+
+    delete total;
     return 0;
 }
