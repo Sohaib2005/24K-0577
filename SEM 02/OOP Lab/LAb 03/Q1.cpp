170e3b929dc4a5be1f77c0237dd77730d0d74f2c
Muhammad Sohaib <164145499+Sohaib2005@users.noreply.github.com>
2025-02-10 23:37:14 +0500

    Update Q1.cpp

--- SEM 02/OOP Lab/LAb 03/Q1.cpp
+++ SEM 02/OOP Lab/LAb 03/Q1.cpp
@@ -1,21 +1,30 @@
-#include <iostream>
-#include <string>
+#include<iostream>
+#include<string>
 using namespace std;
-class User {
-    public:
-    int age;
-    string name;
-    User(string name,int age) {
-        this->name = name;
-        this->age = age;
-    }
-};
-int main() {
-    /*Create a class User with two public fields: int Age and string Name. In the Main method, create an object of class
-User and set Name to &quot;Teo&quot; and Age to 24. Then, output to the screen: &quot;My name is {Name} and I&#39;m {Age} years
-old.&quot; using object fields for Name and Age.*/
-User one("Teo",24);
 
-cout <<"My name is " << one.name << " and I am " << one.age << " years old";
-    return 0;
+class User{
+	public :
+		int age;
+		string name;
+		
+		void get_name(string Name){
+			string name = Name;
+		}
+		
+		void get_age(int Age){
+			int age= Age;
+		}
+		
+		void display_output(){
+			cout<<"My Name is "<<name<<" and I'm "<<age;
+		}	
+};
+
+int main(int argc , char * argv[]){	
+        class User user;       
+	    user.get_name(argv[1]);
+	    user.get_age(stoi(argv[2]));
+	    user.display_output();
+		
+	return 0;
 }
